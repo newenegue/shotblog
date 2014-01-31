8ae10674a4799aee1c1187884d8901ab7a1bad7d
Eugene Wen <newenegue@gmail.com>
2014-01-30 16:58:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shotblog::Application.config.secret_key_base = '14aa42c545c102be9d22570ae423af7d2c64a17fe39e8525ce454bfb9d515b3c39161cf71f4c27fdcba04c3084eef5cf396c22eee4ea639968e3d85b54ac89bc'
